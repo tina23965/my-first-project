44a6171aefd739c7d434539b40afec4d4d48f252
William Lee <ithan0117@gmail.com>
2022-07-14 15:10:59 +0800

    bkbjkjb

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -13,7 +13,7 @@ public class Hello extends HttpServlet {
        
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		PrintWriter out = response.getWriter();
-		out.println("Hello from Sophie~~~~~~~~!!!");
+		out.println("Hello from William");
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
