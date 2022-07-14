ca34b337dd9775436052c160e9bf668ad6f30f8f
09_Tina <t8i2n6a14@gmail.com>
2022-07-14 15:17:39 +0800

    Merge branch 'main' of https://github.com/tina23965/my-first-project
    # Conflicts:
    #	src/main/java/Hello.java
    add test

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -13,7 +13,7 @@ public class Hello extends HttpServlet {
        
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		PrintWriter out = response.getWriter();
-		out.println("Hello from zzz");
+		out.println("Hello from Tina");
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
